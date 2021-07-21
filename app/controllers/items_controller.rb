aaee9965e3a974f8271da933d4220c281fc06e42
Grant Cloyd <dgcloyd@gmail.com>
2021-07-21 11:27:15 -0500

    commit tree 341a31482ae5e5168cf8e1686a1307fc13c0f328

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,40 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :handle_not_found_error
 
   def index
-    items = Item.all
-    render json: items, include: :user
+   if params[:user_id]
+     user = User.find(params[:user_id])
+     render json: user.items
+    else
+     items = Item.all
+     render json: items, include: :user
+    end
+   
+    end
+
+    def show
+      if params[:user_id]
+        item = Item.find(params[:id])
+       
+        render json: item
+      end
+    end
+ 
+  def create 
+  user = User.find(params[:user_id])
+  item = user.items.create(item_params)
+  render json: item, status: :created
   end
+
+
+    private 
+
+    def handle_not_found_error
+      render json: {status: "Error"}, status: :not_found
+    end
+   
+    def item_params
+      params.permit(:name, :description, :price)
+    end
 
 end
